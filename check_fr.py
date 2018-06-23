800738ed955c9b15acd8e6ecb7c117a5c4ba215b
Priyank <priyankmakwana88@gmail.com>
2018-06-24 02:24:54 +0530

    testing stage +1

--- check_fr.py
+++ check_fr.py
@@ -5,16 +5,19 @@ import face_recognition
 picture_of_me = face_recognition.load_image_file("Students/129/10.jpeg")
 my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
 
+picture_of_me2 = face_recognition.load_image_file("Students/129/8.jpeg")
+my_face_encoding2 = face_recognition.face_encodings(picture_of_me)[0]
 # my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
 
-unknown_picture = face_recognition.load_image_file("me.jpg")
+unknown_picture = face_recognition.load_image_file("3.jpg")
 unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
 
 # Now we can see the two face encodings are of the same person with `compare_faces`!
 
-results = face_recognition.compare_faces([my_face_encoding], unknown_face_encoding)
+results = face_recognition.compare_faces([my_face_encoding,my_face_encoding2], unknown_face_encoding)
 
-print  (results)
+print(my_face_encoding)
+print(results)
 if results[0] == True:
     print("It's a picture of me!")
 else:
